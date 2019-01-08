24bb1f5dc68eec783b714bced5e7c72fc68b62d3
andori8 <falcsmike7@gmail.com>
2019-01-07 20:09:14 -0500

    added category model and defined model relations

---
+++ app/controllers/api/v1/quotes_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::QuotesController < ApplicationController
+  def index
+  end
+
+  def add
+  end
+
+  def show
+  end
+end
--- db/migrate/20190108000937_create_quotes.rb
+++ db/migrate/20190108000937_create_quotes.rb
@@ -3,6 +3,7 @@ class CreateQuotes < ActiveRecord::Migration[5.2]
     create_table :quotes do |t|
       t.text :blurb
       t.text :contributor
+      t.integer :category_id
 
       t.timestamps
     end
